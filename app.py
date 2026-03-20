df775ba15508190ec3503c8e56da2f8670a124cf
Alish Bista <abista@devfinity.com>
2026-03-20 08:42:52 +0545

    Modified the source

--- app.py
+++ app.py
@@ -1,37 +1,60 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine, text, URL
+
+
+USER = "readonly_user.tyxjmbptftftcqgozyfc"
+PASSWORD = "your_secure_password"
+HOST = "aws-1-us-east-1.pooler.supabase.com"
+PORT = "6543"
+DBNAME = "postgres"
+
+# Construct the SQLAlchemy connection string
+DATABASE_URL = f"postgresql+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
+
+# Create the SQLAlchemy engine
+engine = create_engine(DATABASE_URL)
 
 # --- Data ---
 @st.cache_data
-def load_data():
-    rng = np.random.default_rng(seed=42)
-    return pd.DataFrame({
-        "month": pd.date_range("2024-01", periods=100, freq="D"),
-        "sales": rng.integers(50, 500, 100),
-        "category": rng.choice(["Electronics", "Clothing", "Food"], 100)
-    })
+def load_country_data():
+    q2 = """
+    SELECT billing_country AS country,
+           COUNT(*)          AS invoice_count,
+           ROUND(SUM(total)::numeric, 2) AS total_spent
+    FROM invoice
+    GROUP BY billing_country
+    ORDER BY total_spent DESC
+    LIMIT 15
+    """
+    return pd.read_sql(q2, con=engine)
 
-df = load_data()
+df_countries = load_country_data()
+
 
 # --- Sidebar filter ---
 st.title("Sales Dashboard")
 
-categories = st.sidebar.multiselect(
-    "Category",
-    options=df["category"].unique(),
-    default=df["category"].unique()
+countries = st.sidebar.multiselect(
+    "Country",
+    options=df_countries["country"].unique(),
+    default=df_countries["country"].unique()
 )
 
 # --- Filter ---
-filtered = df[df["category"].isin(categories)]
+filtered = df_countries[df_countries["country"].isin(countries)]
 
-# --- KPI cards ---
-col1, col2 = st.columns(2)
+g = sns.catplot(
+    data    = filtered,
+    x       = 'total_spent',
+    y       = 'country',
+    kind    = 'bar',
+    palette = 'viridis',
+    height  = 6,
+    aspect  = 1.4
+)
 
-col1.metric("Total Sales", f"${filtered['sales'].sum():,}")
-col2.metric("Avg. Sale",   f"${filtered['sales'].mean():.0f}")
-
-# --- Viz ---
-st.bar_chart(filtered.groupby("category")["sales"].sum())
-st.dataframe(filtered, use_container_width=True)
+st.pyplot(g)
